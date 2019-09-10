4ec46169b0560155c89d1c504dc3b9d3d39c6fa5
phongvantan <hanhphucao7312@gmail.com>
2019-09-10 14:18:03 +0700

    commit cau 2

--- app/src/main/java/com/example/tvp_cau1/MainActivity.java
+++ app/src/main/java/com/example/tvp_cau1/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.tvp_cau1;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-        import android.os.Bundle;
+import android.content.DialogInterface;
+import android.os.Bundle;
 import android.os.TestLooperManager;
 import android.view.View;
 import android.widget.Button;
@@ -15,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
 TextView tv_tk1,tv_tk2;
 EditText et_1,et_2;
 CheckBox cb_ltt;
-Button btn_dn;
+Button btn_dn,btn_thoat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +29,8 @@ Button btn_dn;
         et_2=(EditText)findViewById(R.id.editText_2);
         cb_ltt=(CheckBox)findViewById(R.id.checkBox_ltt);
         btn_dn=(Button)findViewById(R.id.button_dn);
+        btn_thoat=(Button)findViewById(R.id.button_thoat);
+
         btn_dn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -44,5 +48,27 @@ Button btn_dn;
 
 
     });
+        btn_thoat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this,android.R.style.Theme_DeviceDefault_Dialog);
+                builder.setTitle("Thông báo");
+                builder.setMessage("Bạn có muốn thoát không ?");
+                builder.setIcon(android.R.drawable.ic_dialog_alert);
+                builder.setPositiveButton("Có", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        onBackPressed();
+                    }
+                });
+                builder.setNegativeButton("Không", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                builder.show();
+            }
+        });
     }
 }
